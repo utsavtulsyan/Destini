ed57039f0cac3b96b43db895a0e7066b841c06f6
Utsav Tulsyan <utsav.tulsyan@publicissapient.com>
2020-09-26 01:38:24 +0530

    added story functionality and refactored to MVC pattern

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI(story: 0)
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        storyBrain.nextStory(sender.currentTitle!)
+        updateUI(story: storyBrain.storyNumber)
+    }
+    
+    func updateUI(story: Int){
+        storyLabel.text = storyBrain.stories[ story ].title
+        choice1Button.setTitle(storyBrain.stories[ story ].choice1, for: .normal)
+        
+        choice2Button.setTitle(storyBrain.stories[ story ].choice2, for: .normal)
+    }
+    
 }
 
